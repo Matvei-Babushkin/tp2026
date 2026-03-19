0646edae4cfc48e683418d2fefee56c14da749f9
Pavel <pavelgavrilov812@gmail.com>
2026-03-19 12:48:04 +0300

    Update composite_shape.h

--- gavrilov.pavel/T4/composite_shape.h
+++ gavrilov.pavel/T4/composite_shape.h
@@ -7,31 +7,17 @@
 
 class CompositeShape : public Shape {
 private:
-    std::vector<std::unique_ptr<Shape>> shapes;
+    std::vector<std::unique_ptr<Shape>> shapes_;
 
 public:
-    CompositeShape() = default;
-    ~CompositeShape() override = default;
-
-    CompositeShape(const CompositeShape&) = delete;
-    CompositeShape& operator=(const CompositeShape&) = delete;
-
-    CompositeShape(CompositeShape&&) = default;
-    CompositeShape& operator=(CompositeShape&&) = default;
-
-    void addShape(std::unique_ptr<Shape> shape);
+    void addShape(Shape* shape);
 
     double getArea() const override;
     Point getCenter() const override;
     void move(double dx, double dy) override;
     void scale(double factor) override;
     std::string getName() const override;
-
-    void printInfo() const;
-
-    const std::vector<std::unique_ptr<Shape>>& getShapes() const {
-        return shapes;
-    }
+    void print() const override;
 };
 
 #endif
